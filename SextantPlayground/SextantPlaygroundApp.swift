41c4d3b9cca82a94913a4f80c232085d33e99f6f
Aleksandr Syshchenko <a.syshchenko@autodoc.eu>
2026-03-12 17:12:48 +0200

    initial

---
+++ SextantPlayground/SextantPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SextantPlaygroundApp.swift
+//  SextantPlayground
+//
+//  Created by Alexander Syshchenko on 12.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct SextantPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
